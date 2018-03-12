29cfdace71ca25d4d8030f5429be5c9964b14355
janos <janos.mudri@gmail.com>
2018-03-12 12:44:15 +0000

    regController
    registration controller added to handle registration page requests
    Error lbl controls

--- public/app/views/js/app.loginservice.js
+++ public/app/views/js/app.loginservice.js
@@ -1,28 +1,33 @@
 // inject the stuff service into our main Angular module
-angular.module('loginService', ['authService'])
+angular.module('loginService', ['authService', 'userService'])
 // create a controller and inject the Stuff factory
 .controller('userController', function(Auth) {
 		var vm = this;
 
-		vm.credentials = { username: '', password: ''};
-      // alert("loaded " + Auth.isLoggedIn());
-      console.log(Auth.isLoggedIn());
+		vm.error = false;
+		vm.credentials = { username: '', password: ''};           
 
 		// get all the stuff
 		vm.btn_LoginOnClientclick = function(){
-
-			
+		
 			Auth.login(vm.credentials.username, vm.credentials.password)
 			// promise object
 			.success(function(data) {
 				if (Auth.isLoggedIn()){
 					window.location.href = '/';
+				}
+				else{
+					vm.message = data.message;
+					vm.error = true;
 				}				
-				//alert(JSON.stringify(data));
-			// bind the data to a controller variable
-			// this comes from the stuffService
-	 });		
+			 })		
+			 .error(function(data) {
+				vm.message = "An error occured, please try again later"
+				vm.error = true;
+			 });
 		} //end of btnLogin
+
+
 
 		vm.btn_LogOutOnClientClick = function(){
 			Auth.logout();
@@ -30,16 +35,47 @@ angular.module('loginService', ['authService'])
 		}
 
 })
-.controller('logController', function(Auth){
-    //alert("loginController")
-    console.log("logincontroller")
-    var vm = this;
-    vm.message = 'Look! I am an loginservice.';
-    if (!Auth.isLoggedIn()){
-        window.location.href = '/login';
-	   }
-})
 
+.controller('regController', function($http, Auth){
+	
+	var vm = this;	
+	//controls the bootsrap error panel
+	vm.error = false;
+	vm.credentials = { username: '', password: ''};
+
+	
+	vm.btn_RegisterOnClientClick = function(){
+		//post request to do communicate with the api
+		$http.post('api/users', vm.credentials).then(successCallback, errorCallback);					
+	}
+
+	//on success function
+	function successCallback(result){
+		
+		//if the new user created
+		if (result.data.success == true){
+			//automatically logs in the user, and redirects to home pahe
+			Auth.login(vm.credentials.username, vm.credentials.password)			
+				.success(function(data) {
+					if (data.success = true){
+				
+					window.location.href = '/';
+					}
+				});
+		}
+		else {
+			//bootstrap error panel msg & ctrl
+			vm.message = result.data.message
+			vm.error  = true;			
+		}
+
+	};
+
+	function errorCallback(result){
+		vm.message = "An error occured, please try again later"
+		vm.error = true;
+	}
+})
 
 ;
 
