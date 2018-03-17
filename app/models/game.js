15480bc5e334607ce99943723bacdab30e694722
janos <janos.mudri@gmail.com>
2018-03-17 18:14:59 +0000

    GameSchema to store the map as array

---
+++ app/models/game.js
@@ -0,0 +1,17 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+var bcrypt 		 = require('bcrypt-nodejs');
+var id = mongoose.Types.ObjectId()
+
+// user schema 
+var GameSchema   = new Schema({
+   // _id: {type: mongoose.Schema.Types.ObjectId, required:true, auto: true, index: true, unique: true},
+    //_id: { type: mongoose.Schema.Types.ObjectId, auto: true},
+    name: String,
+    players: String,
+    map: { type : Array , "default" : [] }
+	// username: { type: String, required: true, index: { unique: true }},
+	// password: { type: String, required: true, select: false }
+});
+
+module.exports = mongoose.model('Game', GameSchema);
