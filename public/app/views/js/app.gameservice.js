cdd5d7f27e81c343aec185321aa58e4e5128df68
janos <janos.mudri@gmail.com>
2018-04-01 12:27:55 +0100

    map update function & api/me working

--- public/app/views/js/app.gameservice.js
+++ public/app/views/js/app.gameservice.js
@@ -1,8 +1,9 @@
 
-angular.module('gameService', [])
+angular.module('gameService', ['loginService'])
 // contact page controller
-.controller('gameController', function($http) {
+.controller('gameController', function($http, user, Auth) {
     this.message = 'Contact us! JK. This is just a demo.';
+    //this.donutCounter = 0;
     var counter = 0
     var ctx;
     var defSrc = "/app/img/";
@@ -20,36 +21,87 @@ angular.module('gameService', [])
     var imgTreasureChest = new Image(128,128);
     imgTreasureChest.src = treasureSrc + "chestFull.png"
    // var imgGround07 = new Image(100,100);
-   var mapArray = [[0,2,3,4,0,0,0,0,0],[0,3,1,0,0,0,0,0,0],[0,3,1,0,100,0,0,0,0],[0,2,1,0,0,0,0,0,0],[0,0,1,0,0,0,0,0,0],[0,3,1,0,0,0,0,0,0],[0,3,1,0,0,0,0,0,0],[0,3,1,0,0,0,0,0,0],[0,3,1,0,0,0,0,0,0]]
-
+   var mapArrayDefault = [[0,2,3,4,0,0,0,0,0],[0,3,1,0,0,0,0,0,0],[0,3,1,0,3254570,0,0,0,0],[0,2,1,0,0,0,0,0,0],[0,0,1,0,0,0,0,0,0],[0,3,1,0,0,0,0,0,0],[0,3,1,0,0,0,0,0,0],[0,3,1,0,0,0,0,0,0],[0,3,1,0,0,0,0,0,0]]
+    var mapArray = [];
     var positionX = 350;
     var positionY = 400;
     var positionDonuts;
+    var userId;
+
+    donutCounter = 0;
+    
+
+    function getUserHash() {
+        token =  {token: Auth.getToken()}
+        console.log(Auth.getToken())
+        $http.get('api/me', token).then(successCallback, errorCallback);	
+        function successCallback(result){
+            console.log(result)
+            console.log( result.data.username);
+            console.log(getHash(result.data.username));
+            userId =getHash(result.data.username);
+            return this.userId;
+        //  return null;
+         }     
+         function errorCallback(result){
+             console.log( result)
+             return null;
+             // vm.message = "An error occured, please try again later"
+             // vm.error = true;
+         }
+    }
+
+
+    function getMapArray(){
+        $http({
+            url: 'api/games/5aa67fc352cb9c2821d71b7d', 
+            method: "GET",
+            params: {token: Auth.getToken()}
+         }).then(successCallback, errorCallback);	              
+        //on success function
+         function successCallback(result){
+           mapArray = result.data.map
+           drawMap(result.data.map);
+           return this.mapArray;
+         }     
+         function errorCallback(result){
+             // vm.message = "An error occured, please try again later"
+             // vm.error = true;
+         }
+    }
 
     function getCtx(){
         var c = document.getElementById('myCanvas');  
         return c.getContext("2d");
     }
 
-
+ 
     this.load = function() {
         var img = document.getElementById("homer");
         ctx= getCtx();
-     //   ctx.drawImage(imgBackground, 0,0, 700, 500); 
-        ctx.drawImage(imgHomer, positionX, positionY,50,80);  
-        ctx.drawImage(imgDonut, 50, 50, 40,40);
         ctx.stroke();
+
+        var userId = getUserHash();
+        var mapArray = getMapArray();
+
+            'use strict';
+            var refreshRate = 4000;    // time in millisec
+            window.setInterval(function () {
+               getMapArray();
+            }, refreshRate);
     }
 
+    function getHash(input){
+        var hash = 0, len = input.length;
+        for (var i = 0; i < len; i++) {
+          hash  = ((hash << 5) - hash) + input.charCodeAt(i);
+          hash |= 0; // to 32bit integer
+        }
+        return hash;
+      }
 
-
-    function reDraw(posHomer, posDonuts){
-        ctx = getCtx();
-  
-        drawMap();
-    }
-
-    function drawMap(){
+    function drawMap(mapArray){
+        //console.log(mapArray)
         ctx = getCtx();
         var x =0;
         var y=0;
@@ -58,13 +110,12 @@ angular.module('gameService', [])
             for (j=0; j <mapArray[i].length; j++){
                 ctx.drawImage(imgGround05, x,y,50,50)
                 if (mapArray[i][j] == 1){
-                   
                     ctx.drawImage(imgDonut, x+10, y+10, 30,30)
                 }
                 else if (mapArray[i][j] == 2) {
                     ctx.drawImage(imgTreasureChest, x+10, y+10, 30,30)
                 }
-                else if (mapArray[i][j] == 100) {
+                else if (mapArray[i][j] == userId) {
                     ctx.drawImage(imgHomer, x+10, y+10, 30,30)
                 }
                 
@@ -72,14 +123,14 @@ angular.module('gameService', [])
             }
             y= 0;
             x += 50;
+            ctx.drawImage(imgGround05, positionX, positionY,30,30);  
         }
-      //  ctx.drawImage(imgHomer, positionX, positionY,30,30);  
+       
         
 
     }
 
     function modifyMap(movement){
-        var userId = 100;
         loop1:
         for (i=0; i < mapArray.length; i++){
            loop2:
@@ -87,64 +138,91 @@ angular.module('gameService', [])
                 if (mapArray[i][j] == userId){
                     
                     if (i >= 1 && movement === "left" ){
+                        
+                        updateCounters(mapArray[i-1][j])
                         mapArray[i-1][j] = userId;
+                        mapArray[i][j] = 0;
                         break loop1;
                     }
                     if (i < mapArray.length-1 && movement === "right"){
-                        mapArray[i+1][j] = userId;                        
+                        updateCounters(mapArray[i+1][j])
+                        mapArray[i+1][j] = userId;   
+                        mapArray[i][j] = 0;             
                         break loop1;                     
                     }
                     if (j > 0 && movement === "forward"){
-                        mapArray[i][j-1] = userId;                        
+                        updateCounters(mapArray[i][j-1])
+                        mapArray[i][j-1] = userId;     
+                        mapArray[i][j] = 0;           
                         break loop1;
                     }
                     if (j < mapArray[j].length-1 && movement === "back"){
-                        mapArray[i][j+1] = userId;                        
+                        updateCounters(mapArray[i][j+1])
+                        mapArray[i][j+1] = userId; 
+                        mapArray[i][j] = 0;                      
                         break loop1;
-                    }
-                    mapArray[i][j] = 0;
-                }
+                    }                    
+                }                                                
             }
         }      
         updateGameDB();
     }
 
+    updateCounters = function(value) {
+        console.log("updatecounters " + value)
+        this.message = "updated for " + value
+        this.message = "donuts " + donutCounter;
+        if (value == 1){
+            donutCounter +=1;
+           
+        }
+    }
+
+    // function updateCounters(value){
+
+
+    // }
+
     function updateGameDB(){
         var vm = this;        
-        vm.game = { name: 'zsotika', players: "5a8ab9b69b10d304a8a3eff0", map: JSON.stringify(mapArray)};
-        $http.put('api/games/5aa67fc352cb9c2821d71b7d', vm.game).then(successCallback, errorCallback);	
+        vm.game = { name: 'zsotika', players: "5a8ab9b69b10d304a8a3eff0", map: mapArray};
+        var header =  { "x-access-token": Auth.getToken()}
+        $http.put('api/games/5aa67fc352cb9c2821d71b7d', vm.game, header).then(successCallback, errorCallback);	
  
-	//on success function
-	function successCallback(result){
-        drawMap()
-		console.log("success " +  result);
-	}
+            //on success function
+            function successCallback(result){
+              //  drawMap()
+                console.log("success update");
+                console.log(result);
+            }
 
-	function errorCallback(result){
-        console.log("fail " + result)
-	}      
+            function errorCallback(result){
+                console.log("fail " + result)
+            }      
     }
 
     this.forward = function() {
         counter +=1;
-        this.message = "msg new" + counter;  
+     //   this.message = "msg new" + counter; 
+       // console.log(mapArray) 
+     //   console.log(userId)
         modifyMap("forward") 
         
     }
 
     this.back = function() {
         counter -=1;
-        this.message = "minus" + counter;
+       // this.message = "minus" + counter;
         modifyMap("back");
     }
 
     this.left = function () {
-        this.message = "left ";
+        //this.message = "left ";
         modifyMap("left");    
     }
 
     this.right = function() {
-        this.message = "right";
+        //this.message = "right";
         modifyMap("right");    
     }
 
@@ -152,7 +230,7 @@ angular.module('gameService', [])
         var vm = this;
 
         $http.get('api/games/5aa67fc352cb9c2821d71b7d').then(successCallback, errorCallback);	
-   	//on success function
+   	    //on success function
 	    function successCallback(result){
            console.log( result.data.map);
         }     
@@ -164,40 +242,21 @@ angular.module('gameService', [])
     }
 
 
-
     this.gameTest = function() {
-       
-        var vm = this;
-        var mapArray = [[1,2,3],[0,0,0],[1,0,0]]
-        vm.game = { name: 'zsotika', players: "gazsi2", map: JSON.stringify(mapArray)};
-        $http.put('api/games/5aa67fc352cb9c2821d71b7d', vm.game).then(successCallback, errorCallback);	
+        var vm = this;        
+        vm.game = { name: 'zsotika', players: "5a8ab9b69b10d304a8a3eff0", map: mapArrayDefault};
+        var header =  { "x-access-token": Auth.getToken()}
+        $http.put('api/games/5aa67fc352cb9c2821d71b7d', vm.game, header).then(successCallback, errorCallback);	
  
-	//on success function
-	function successCallback(result){
-		console.log("success " +  result);
-		// //if the new user created
-		// if (result.data.success == true){
-		// 	//automatically logs in the user, and redirects to home pahe
-		// 	Auth.login(vm.credentials.username, vm.credentials.password)			
-		// 		.success(function(data) {
-		// 			if (data.success = true){
-				
-		// 			window.location.href = '/';
-		// 			}
-		// 		});
-		// }
-		// else {
-		// 	//bootstrap error panel msg & ctrl
-		// 	vm.message = result.data.message
-		// 	vm.error  = true;			
-		// }
+            //on success function
+            function successCallback(result){
+               // drawMap()
+                console.log("success update");
+                console.log(result);
+            }
 
-	}
-
-	function errorCallback(result){
-        console.log("fail " + result)
-		// vm.message = "An error occured, please try again later"
-		// vm.error = true;
-	}
+            function errorCallback(result){
+                console.log("fail " + result)
+            }     
     }
 });
