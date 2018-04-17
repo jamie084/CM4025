daac79c63f9ca648ad0163f59e0f6cdc1d55b03c
janos <janos.mudri@gmail.com>
2018-04-17 19:51:02 +0100

    schema changed

--- app/models/game.js
+++ app/models/game.js
@@ -8,7 +8,7 @@ var GameSchema   = new Schema({
    // _id: {type: mongoose.Schema.Types.ObjectId, required:true, auto: true, index: true, unique: true},
     //_id: { type: mongoose.Schema.Types.ObjectId, auto: true},
     name: String,
-    players: String,
+    players: { type : Array , "default" : [] },
     map: { type : Array , "default" : [] }
 	// username: { type: String, required: true, index: { unique: true }},
 	// password: { type: String, required: true, select: false }
