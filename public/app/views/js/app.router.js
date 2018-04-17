222a9f6265d97e09f0202b241003b5b7c6c00a4d
janos <janos.mudri@gmail.com>
2018-04-17 19:53:35 +0100

    selectgamecontroller added

--- public/app/views/js/app.router.js
+++ public/app/views/js/app.router.js
@@ -1,13 +1,14 @@
 angular.module('routerApp', ['routerRoutes', 'loginService',  'gameService', 'ngAnimate'])
 
+
 // create the controller and inject Angular's 
 // this will be the controller for the ENTIRE site
 .controller('mainController', function() {
 
-	var vm = this;
+	var vm = this;	
 
-    	// create a bigMessage variable to display in our view
-    	vm.bigMessage = 'A smooth sea never made a skilled sailor.';
+  	// create a bigMessage variable to display in our view
+   	vm.bigMessage = 'A smooth sea never made a skilled sailor.';
 
 })
 
@@ -43,11 +44,47 @@ angular.module('routerApp', ['routerRoutes', 'loginService',  'gameService', 'ng
     	vm.message = 'Contact us! JK. This is just a demo.';
 })
 
+.controller('selectGameController', function($http, Auth){
+	var vm = this;
+	vm.message = 'select a game to play';
+
+	vm.games = [];
+
+	$http({
+		url: 'api/games/', 
+		method: "GET",
+		params: {token: Auth.getToken()}
+	 }).then(successCallback, errorCallback);	              
+	//on success function
+	 function successCallback(result){
+		 console.log(result.data[0])
+	   for (var i=0; i < result.data.length; i++){
+		   vm.games.push({
+			name: result.data[i].name,
+			players: result.data[i].players.length,
+			_id: result.data[i]._id,
+			nerdness: 7
+		});
+	   }
+
+	  //console.log(mapArray)
+	   return this.mapArray;
+	 }     
+	 function errorCallback(result){
+		 // vm.message = "An error occured, please try again later"
+		 // vm.error = true;
+	 }
+	
+
+})
+
 // contact page controller
 .controller('registerController', function() {
 
 	var vm = this;
 
 		vm.message = 'register contrll';
+
+		
 		
 });
