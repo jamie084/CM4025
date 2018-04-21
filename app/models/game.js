5a66faa8008864f28477e0f3dcf756379302a9fc
janos <janos.mudri@gmail.com>
2018-04-21 15:00:09 +0100

    base map layer added

--- app/models/game.js
+++ app/models/game.js
@@ -2,14 +2,17 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 var bcrypt 		 = require('bcrypt-nodejs');
 var id = mongoose.Types.ObjectId()
+mongoose.set('debug', true);
 
 // user schema 
 var GameSchema   = new Schema({
    // _id: {type: mongoose.Schema.Types.ObjectId, required:true, auto: true, index: true, unique: true},
     //_id: { type: mongoose.Schema.Types.ObjectId, auto: true},
+    
     name: String,
     players: { type : Array , "default" : [] },
-    map: { type : Array , "default" : [] }
+    map: { type : Array , "default" : [] },
+    basemap: { type : Array , "default" : [] }
 	// username: { type: String, required: true, index: { unique: true }},
 	// password: { type: String, required: true, select: false }
 });
